300e070ff7ee021e76ebc4dd218c12f5aab8507d
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-22 21:18:39 +0100

    use username to local storage

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './profile.css'
 import GridBooks from '../../components/grid_books/GridBooks';
 
 const Profile = () => {
+  const [userName, setUserName] = useState(null)
 
-    const user = {
-        name: 'usuario1',
-    };
+    useEffect(() => {
+      setUserName(localStorage.getItem('user_name'))
+    }, [])
+    
 
   return (
     <div className='container_profile_view'>
-        <h2>{user.name}</h2>
-        <div className="container_user_image">{user.name[0]}</div>
+        <h2>{userName ? userName : '..'}</h2>
+        <div className="container_user_image">{userName ? userName[0] : '..'}</div>
         <h2>Libros disponibles</h2>
         <GridBooks/>
     </div>
