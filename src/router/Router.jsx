6acf34e44b1c24d6fce42870b137828f00e9cb5d
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-22 13:03:28 +0100

    inicializate proyect

---
+++ src/router/Router.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../views/home/Home";
+
+const Router = () => {
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/pagina2" element={<h1>pagina2</h1>} />
+         </Routes>
+      </BrowserRouter>
+   );
+};
+
+export default Router;
