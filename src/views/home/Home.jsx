365c152e342423c12a1c94c37ec9aa6a525a03b1
Hildacaro <hildacarodcr@gmail.com>
2023-02-22 17:14:41 +0100

    Design and operation of the form

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import Form from '../../components/form/Form'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Form/>
+        
         <h1>Vista home</h1>
     </div>
   )
