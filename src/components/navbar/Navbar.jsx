3fbd70ed356a46dc5060127a511c980e2e12b187
unknown <camilaruiz17@gmail.com>
2023-02-23 14:19:19 +0100

    search done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,8 +5,32 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import axios from "axios";
+import {useEffect, useState} from 'react';
 
 function NavBar() {
+
+  const [usuarios, setUsuarios]= useState([]);
+  const [tablaUsuarios, setTablaUsuarios]= useState([]);
+  const [busqueda, setBusqueda]= useState("");
+
+
+const handleChange = (e) => {
+  setBusqueda(e.target.value);
+}
+
+
+  const handleSearch = async () => {
+    await axios.get("http://localhost:8000/api/search/"+busqueda)
+  .then(response=>{
+    /*setUsuarios(response.data);
+    setTablaUsuarios(response.data);*/
+    console.log(response.data)
+  }).catch(error=>{
+    console.log(error);
+  })
+  }
+
   return (
     <Navbar className='navBarColor' expand="lg">
       <Container fluid>
@@ -24,8 +48,9 @@ function NavBar() {
               placeholder="Search"
               className="me-2"
               aria-label="Search"
+              onChange={handleChange}
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-success" onClick={handleSearch}>Search</Button>
           </Form>
             <Nav.Link href="#action1">Home</Nav.Link>
             <Nav.Link href="#action2">My Books</Nav.Link>
