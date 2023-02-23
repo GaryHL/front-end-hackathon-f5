1ab6b8b02c466497ace58da9da3b024788d57988
Juan-Little <juanbenitodevalle@gmail.com>
2023-02-23 17:15:26 +0100

    photo  gallery

--- src/components/writers/Writers.jsx
+++ src/components/writers/Writers.jsx
@@ -1,38 +1,33 @@
 import React from 'react'
-import wr1 from '../../assets/images/wr1.jpg'
-import wr2 from '../../assets/images/wr2.jpg'
-import wr3 from '../../assets/images/wr3.jpg'
-import wr4 from '../../assets/images/wr4.jpg'
-import wr5 from '../../assets/images/wr5.jpg'
-import wr6 from '../../assets/images/wr6.jpg'
+import b12 from '../../assets/images/b12.jpg'
 
 import { Container, Row, Col, Image } from 'react-bootstrap'
 
 function Writers() {
   return (
    <Container>
-   <h1 className="text"> TOP BOOKS</h1>
-         <Row className="justify-content-center" >
+   <h1 className="text mt-5"> TOP BOOKS</h1>
+         <Row className="justify-content-center mb-5 gap-5 mt-5" >
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={wr1} fluid className="mb-3" style={{ height: '400px' }}/>
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }}/>
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={wr2} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={wr3} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
            </Col>
          </Row>
    
-         <Row className="justify-content-center" >
+         <Row className="justify-content-center mt-5 mb-5 gap-5">
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={wr4} fluid className="mb-3" style={{ height: '400px' }}/>
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }}/>
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={wr5} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={wr6} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
            </Col>
          </Row>
        </Container>
