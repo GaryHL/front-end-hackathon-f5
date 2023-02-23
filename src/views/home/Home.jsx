ca586bee817c3b758fef33e80336f29f65a01498
pohengchan <pohengchan@gmail.com>
2023-02-23 01:18:05 +0100

    add description

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Releases from '../../components/Releases/Releases'
+import Description from '../../components/description/Description'
+import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Releases/>
+        <Description/>
         <h1>V</h1>
+        <Footer/>
     </div>
   )
 }
