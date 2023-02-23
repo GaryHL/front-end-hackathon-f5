6e3799ceb11796c1b52f382bc8bb554c5b3f0033
Juan-Little <juanbenitodevalle@gmail.com>
2023-02-23 14:34:44 +0100

    photos for the gallery

--- src/components/writers/Writers.jsx
+++ src/components/writers/Writers.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import b12 from '../../assets/images/b12.jpg'
+import wr1 from '../../assets/images/wr1.jpg'
+import wr2 from '../../assets/images/wr2.jpg'
+import wr3 from '../../assets/images/wr3.jpg'
+import wr4 from '../../assets/images/wr4.jpg'
+import wr5 from '../../assets/images/wr5.jpg'
+import wr6 from '../../assets/images/wr6.jpg'
+
 import { Container, Row, Col, Image } from 'react-bootstrap'
 
 function Writers() {
