43cc9d6ac54f6723281f7e882f9197c0a2853556
Juan-Little <juanbenitodevalle@gmail.com>
2023-02-23 17:34:25 +0100

    photo  gallery

--- src/components/writers/Writers.jsx
+++ src/components/writers/Writers.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import b12 from '../../assets/images/b12.jpg'
+import wr1 from '../../assets/images/wr1.jpg'
 
 import { Container, Row, Col, Image } from 'react-bootstrap'
 
@@ -9,25 +9,25 @@ function Writers() {
    <h1 className="text mt-5"> TOP BOOKS</h1>
          <Row className="justify-content-center mb-5 gap-5 mt-5" >
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }}/>
+             <Image src={wr1} fluid className="mb-3" style={{ height: '300px' }} roundedCircle/>
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }}  roundedCircle />
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} roundedCircle />
            </Col>
          </Row>
    
          <Row className="justify-content-center mt-5 mb-5 gap-5">
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }}/>
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} roundedCircle/>
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} roundedCircle />
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} />
+             <Image src={b12} fluid className="mb-3" style={{ height: '300px' }} roundedCircle />
            </Col>
          </Row>
        </Container>
