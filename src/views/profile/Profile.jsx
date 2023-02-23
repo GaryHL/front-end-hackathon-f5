6da3b89b337f7ed0d7844ce1a13bb04549886f60
Hildacaro <hildacarodcr@gmail.com>
2023-02-23 13:41:59 +0100

    Form Book

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './profile.css'
 import GridBooks from '../../components/grid_books/GridBooks';
+import FormBooks from '../../components/form_books/FormBooks';
 
 const Profile = () => {
   const [userName, setUserName] = useState(null)
@@ -16,6 +17,7 @@ const Profile = () => {
         <div className="container_user_image">{userName ? userName[0] : '..'}</div>
         <h2>Libros disponibles</h2>
         <GridBooks/>
+        <FormBooks/>
     </div>
   )
 } 
