1d389b4e0e6281a9d3af4a3cf5388a696703cf2e
Juan-Little <juanbenitodevalle@gmail.com>
2023-02-23 14:39:42 +0100

    photos for the gallery

--- src/components/writers/Writers.jsx
+++ src/components/writers/Writers.jsx
@@ -14,25 +14,25 @@ function Writers() {
    <h1 className="text"> TOP BOOKS</h1>
          <Row className="justify-content-center" >
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '400px' }}/>
+             <Image src={wr1} fluid className="mb-3" style={{ height: '400px' }}/>
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={wr2} fluid className="mb-3" style={{ height: '400px' }} />
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={wr3} fluid className="mb-3" style={{ height: '400px' }} />
            </Col>
          </Row>
    
          <Row className="justify-content-center" >
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '400px' }}/>
+             <Image src={wr4} fluid className="mb-3" style={{ height: '400px' }}/>
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={wr5} fluid className="mb-3" style={{ height: '400px' }} />
            </Col>
            <Col xs={12} md={6} lg={3} className="text-center">
-             <Image src={b12} fluid className="mb-3" style={{ height: '400px' }} />
+             <Image src={wr6} fluid className="mb-3" style={{ height: '400px' }} />
            </Col>
          </Row>
        </Container>
