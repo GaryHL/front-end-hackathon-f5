ddfc3a8990f243d8eaaa256fbeaca0ac6396aa18
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-24 02:14:34 +0100

    modify carrousel style

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,49 +1,44 @@
-import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import './carousel.css'
 
 function IndividualIntervalsExample() {
-  return (
-    <Carousel variant="dark">
-      <Carousel.Item interval={1000}>
-        <div>
-          <img
-            className="d-block w-100"
-            src="./src\assets\images\Rectangle 234.png"
-            alt="First slide"
-          />
-        </div>
-        <Carousel.Caption>
-          <h3>Unique Books</h3>
-          <p>
-            RareBooks is a marketplace where you find the most unique
-            second-hand books, research-technical manual and special edition
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="./src\assets\images\Rectangle 234 (1).png"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>What you can do</h3>
-          <p>You have access to the most especials vendors</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.unsplash.com/3/alejandroescamilla-book.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1562&q=80"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Get in contact</h3>
-          <p>Welcome to our community of book connoisseurs!</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  )
+   const dataCarrousel = [
+      {
+         imageSrc: "https://images.pexels.com/photos/3207628/pexels-photo-3207628.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+         captionTitle: "Unique Books",
+         captionDescription:
+            "RareBooks is a marketplace where you find the most unique second-hand books, research-technical manual and special edition",
+      },
+      {
+         imageSrc: "https://images.pexels.com/photos/5939401/pexels-photo-5939401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+         captionTitle: "What you can do",
+         captionDescription: "You have access to the most especials vendors",
+      },
+      {
+         imageSrc:
+            "https://images.pexels.com/photos/114820/pexels-photo-114820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+         captionTitle: "Get in contact",
+         captionDescription: "Welcome to our community of book connoisseurs!",
+      },
+   ];
+   return (
+      <Carousel variant="light" className="container_carousel" style={{ borderRadius:'30px'}}>
+         {dataCarrousel.map((item) => {
+            return (
+               <Carousel.Item interval={2000} style={{background:'black', borderRadius:'30px',overflow:'hidden'}}>
+                  <div style={{width:'100%', maxHeight:'500px'}}>
+                     <img className="d-block " src={item.imageSrc} alt="First slide" style={{opacity:0.4, width:'100%', borderRadius:'30px'}} />
+                  </div>
+                  <Carousel.Caption>
+                     <h3>{item.captionTitle}</h3>
+                     <p>{item.captionDescription}</p>
+                  </Carousel.Caption>
+               </Carousel.Item>
+            );
+         })}
+      </Carousel>
+   );
 }
 
-export default IndividualIntervalsExample
+export default IndividualIntervalsExample;
